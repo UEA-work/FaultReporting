0ef79d86df9c4e104d2cbf652fb5fd7531593d53
unknown <sumera.ather12@gmail.com>
2018-12-11 14:48:29 +0000

    latest

--- src/pages/employee-login/employee-login.ts
+++ src/pages/employee-login/employee-login.ts
@@ -1,7 +1,13 @@
 import { Component } from "@angular/core";
 import { IonicPage, NavController, NavParams } from "ionic-angular";
 import { EmployeeReportFaultFormPage } from "../employee-report-fault-form/employee-report-fault-form";
-import { FormBuilder, FormGroup } from "@angular/forms";
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  AbstractControl
+} from "@angular/forms";
+
 /**
  * Generated class for the EmployeeLoginPage page.
  *
@@ -16,19 +22,33 @@ import { FormBuilder, FormGroup } from "@angular/forms";
 })
 export class EmployeeLoginPage {
   credentialsForm: FormGroup;
+  email: any;
+  password: any;
+
   constructor(
     public navCtrl: NavController,
     private formBuilder: FormBuilder,
     public navParams: NavParams
   ) {
     this.credentialsForm = this.formBuilder.group({
-      email: [""],
-      password: [""]
+      email: ["", [Validators.pattern(".+@.+..+"), Validators.required]],
+      password: ["", [Validators.required, Validators.minLength(8)]]
     });
+    this.email = this.credentialsForm.controls["email"];
+    this.password = this.credentialsForm.controls["password"];
   }
 
   employeeLogin() {
+    if (this.credentialsForm.valid) {
+      console.log(" Email Id:", +this.email);
+      console.log("employeeLogin() => inside IF statement ");
+    }
+    console.log("in employeeLogin() ");
     this.navCtrl.setRoot(EmployeeReportFaultFormPage);
+  }
+  onForgotPassword() {}
+  clear() {
+    this.credentialsForm.reset();
   }
 
   ionViewDidLoad() {
